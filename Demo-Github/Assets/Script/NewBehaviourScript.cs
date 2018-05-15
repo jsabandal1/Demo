4845bdc42c7df04568d0bc0584a946d03ef9d03b
Justin Haimila <jhaimila1@studentmail.nait.ca>
2018-05-14 19:03:52 -0600

    Made a dick script
    it doesn't work

---
+++ Demo-Github/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    //this is for the dick
+
+    int speed = 10;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+		
+	}
+}
